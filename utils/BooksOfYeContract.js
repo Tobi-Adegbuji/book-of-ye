efee9f82ab5a2662d3fb159fa0d1ad1d345cd62d
johnadegbuji <johnadegbuji1@gmail.com>
2022-05-08 18:15:53 -0700

    changed contract

--- utils/BooksOfYeContract.js
+++ utils/BooksOfYeContract.js
@@ -4,7 +4,7 @@ import BooksOfYeA from "./BooksOfYeA.json";
 const instance = new web3.eth.Contract(
     BooksOfYeA.abi,
     //Rink
-    '0x5741C8e37EBB8074316a6307D4Eda1c86b20Cb99'
+    '0x1E2b4458C364063E84026d09df6193071EB73B6E'
     //Mainnet
     // "0x63a500Db4C3cAdBa7C663e4C7A0F92c1D75274f3"
   );
--- utils/BoyContract.js
+++ utils/BoyContract.js
@@ -4,7 +4,7 @@ import { Contract } from '@ethersproject/contracts'
 import { ethers } from 'ethers'
  
 //721A Rinkeby Contract
- const contractAddress = '0x5741C8e37EBB8074316a6307D4Eda1c86b20Cb99'
+ const contractAddress = '0x1E2b4458C364063E84026d09df6193071EB73B6E'
  
 // '0x63a500Db4C3cAdBa7C663e4C7A0F92c1D75274f3'
 // '0x580b6e863567402E8AEDFf49391d4F26E9E34151'
